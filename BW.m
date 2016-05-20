74e91bff6b4de72c7d38526b5607c3c26f932be1
Saman <samansarraf@ieee.org>
2016-05-20 08:06:03 -0700

    noexcel
    Added ‘noexcel’ capability. By calling “BW noexcel”, the toolbox only saves the MATLAB structure that saves time and huge computation. It enables users to perform their own results manipulation if no excel files are required. 
    Changes: Modifies BW.m , added ReportGen1_nex , ReportGen2_nex and ReportGen3_nex.

--- BW.m
+++ BW.m
@@ -1,4 +1,4 @@
-function BW
+function BW(nex)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%                                                                     %%%
 %%%                                                                     %%%
@@ -37,13 +37,70 @@ nofSubj = size(D.Z,3);
 D.Z = D.Z(1:sr,1:sr,1:nofSubj);
 Rois = Rois(1,1:sr);
 Rois2 = Rois(1,I);
-if parallel_flag == 1
-    if any(strcmp('Parallel Computing Toolbox',{v.Name}))
-        disp('Calculating Metrics & Generating Reports ...')
-        dataP = D.Z;
-        matlabpool
-        parfor i = 1 : nofSubj
-        data = dataP(:,:,i);
+%%%%%%%%%%%%% No Excel %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+if nex =='noexcel'
+    if parallel_flag == 1
+        if any(strcmp('Parallel Computing Toolbox',{v.Name}))
+            disp('Calculating Metrics & Generating Reports ...')
+            dataP = D.Z;
+            matlabpool
+            parfor i = 1 : nofSubj
+                data = dataP(:,:,i);
+                data(isnan(data)) = 0;
+                dataOrdered = data(I,I);
+                L{i} = ModuleGen(nofModule,dataOrdered,J,Rois,Rois2);
+                L{i} = BWCount(L{i},thresh);
+                L{i} = BWAMean(L{i});
+                FileName = ['Subject_' num2str(i),'.xlsx'];
+                ReportGen1_nex(Rois2,L{i},dataOrdered)
+                end
+                matlabpool close
+        end
+    else
+        h = waitbar(0,'Calculating Metrics & Generating Reports ...');
+        for i = 1 : nofSubj
+            waitbar (i/nofSubj,h)  
+            data = D.Z(:,:,i);
+            data(isnan(data)) = 0;
+            dataOrdered = data(I,I);
+            L{i} = ModuleGen(nofModule,dataOrdered,J,Rois,Rois2);
+            L{i} = BWCount(L{i},thresh);
+            L{i} = BWAMean(L{i});
+            FileName = ['Subject_' num2str(i),'.xlsx'];
+            ReportGen1_nex(Rois2,L{i},dataOrdered)
+        end
+        close(h)
+    end
+    disp('Summary...')
+    ReportGen2_nex (L) %Summary
+    disp('Extracting Correlation values for all subjects ...')
+    All_Corr = ReportGen3_nex (D.Z,I,Rois2);%All correlation matrices in one excel file
+    save('BW_results.mat','L','All_Corr')
+    disp('B/W Analysis is done successfully!')
+else
+
+    if parallel_flag == 1
+        if any(strcmp('Parallel Computing Toolbox',{v.Name}))
+            disp('Calculating Metrics & Generating Reports ...')
+            dataP = D.Z;
+            matlabpool
+            parfor i = 1 : nofSubj
+            data = dataP(:,:,i);
+            data(isnan(data)) = 0;
+            dataOrdered = data(I,I);
+            L{i} = ModuleGen(nofModule,dataOrdered,J,Rois,Rois2);
+            L{i} = BWCount(L{i},thresh);
+            L{i} = BWAMean(L{i});
+            FileName = ['Subject_' num2str(i),'.xlsx'];
+            ReportGen1(FileName,Rois2,L{i},dataOrdered)
+            end
+            matlabpool close
+        end
+    else
+        h = waitbar(0,'Calculating Metrics & Generating Reports ...');
+        for i = 1 : nofSubj
+        waitbar (i/nofSubj,h)  
+        data = D.Z(:,:,i);
         data(isnan(data)) = 0;
         dataOrdered = data(I,I);
         L{i} = ModuleGen(nofModule,dataOrdered,J,Rois,Rois2);
@@ -52,27 +109,13 @@ if parallel_flag == 1
         FileName = ['Subject_' num2str(i),'.xlsx'];
         ReportGen1(FileName,Rois2,L{i},dataOrdered)
         end
-        matlabpool close
+        close(h)
     end
-else
-    h = waitbar(0,'Calculating Metrics & Generating Reports ...');
-    for i = 1 : nofSubj
-    waitbar (i/nofSubj,h)  
-    data = D.Z(:,:,i);
-    data(isnan(data)) = 0;
-    dataOrdered = data(I,I);
-    L{i} = ModuleGen(nofModule,dataOrdered,J,Rois,Rois2);
-    L{i} = BWCount(L{i},thresh);
-    L{i} = BWAMean(L{i});
-    FileName = ['Subject_' num2str(i),'.xlsx'];
-    ReportGen1(FileName,Rois2,L{i},dataOrdered)
-    end
-    close(h)
+    disp('Summary...')
+    ReportGen2 (L,'Summary_report.xlsx') %Summary
+    disp('Extracting Correlation values for all subjects ...')
+    All_Corr = ReportGen3 (D.Z,I,Rois2,'CorrMat_allsubjects.xlsx');%All correlation matrices in one excel file
+    save('BW_results.mat','L','All_Corr')
+    disp('B/W Analysis is done successfully!')
 end
-disp('Summary...')
-ReportGen2 (L,'Summary_report.xlsx') %Summary
-disp('Extracting Correlation values for all subjects ...')
-All_Corr = ReportGen3 (D.Z,I,Rois2,'CorrMat_allsubjects.xlsx');%All correlation matrices in one excel file
-save('BW_results.mat','L','All_Corr')
-disp('B/W Analysis is done successfully!')
 end
