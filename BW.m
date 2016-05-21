75f2705b6a532c36ad2c4ec80ad2735500c4604e
Saman <samansarraf@ieee.org>
2016-05-21 00:03:47 -0700

    summary
    summary capability added to BW 
    syntax BW summary will output excel files : MATLAB file, summary and all corr mat - no indvidual excel files

--- BW.m
+++ BW.m
@@ -6,7 +6,12 @@ function BW(nex)
 %%% Saman Sarraf - 2015                                                 %%%
 %%% samansarraf@ieee.org                                                %%%
 %%% ssarraf.rotman@gmail.com                                            %%%
+%%% https://github.com/samansarraf/BW_Toolbox.git                       %%%
 %%%                                                                     %%%
+%%% Syntax:                                                             %%% 
+%%%    BW                                                               %%% 
+%%%    BW noexcel                                                       %%% 
+%%%    BW summary                                                       %%% 
 %%%                                                                     %%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 clc 
@@ -37,9 +42,14 @@ nofSubj = size(D.Z,3);
 D.Z = D.Z(1:sr,1:sr,1:nofSubj);
 Rois = Rois(1,1:sr);
 Rois2 = Rois(1,I);
+if nargin < 1 
+    nex = 'empty';
+end
+
 %%%%%%%%%%%%% No Excel %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-if nex =='noexcel'
+if strcmp(nex,'noexcel')
     if parallel_flag == 1
+        
         if any(strcmp('Parallel Computing Toolbox',{v.Name}))
             disp('Calculating Metrics & Generating Reports ...')
             dataP = D.Z;
@@ -77,6 +87,48 @@ if nex =='noexcel'
     All_Corr = ReportGen3_nex (D.Z,I,Rois2);%All correlation matrices in one excel file
     save('BW_results.mat','L','All_Corr')
     disp('B/W Analysis is done successfully!')
+    
+    
+elseif strcmp(nex,'summary')
+    if parallel_flag == 1
+        if any(strcmp('Parallel Computing Toolbox',{v.Name}))
+            disp('Calculating Metrics & Generating Reports ...')
+            dataP = D.Z;
+            matlabpool
+            parfor i = 1 : nofSubj
+                data = dataP(:,:,i);
+                data(isnan(data)) = 0;
+                dataOrdered = data(I,I);
+                L{i} = ModuleGen(nofModule,dataOrdered,J,Rois,Rois2);
+                L{i} = BWCount(L{i},thresh);
+                L{i} = BWAMean(L{i});
+                FileName = ['Subject_' num2str(i),'.xlsx'];
+                ReportGen1_nex(Rois2,L{i},dataOrdered)
+            end
+            matlabpool close
+        end     
+    else
+        h = waitbar(0,'Calculating Metrics & Generating Reports ...');
+        for i = 1 : nofSubj
+            waitbar (i/nofSubj,h)  
+            data = D.Z(:,:,i);
+            data(isnan(data)) = 0;
+            dataOrdered = data(I,I);
+            L{i} = ModuleGen(nofModule,dataOrdered,J,Rois,Rois2);
+            L{i} = BWCount(L{i},thresh);
+            L{i} = BWAMean(L{i});
+            FileName = ['Subject_' num2str(i),'.xlsx'];
+            ReportGen1_nex(Rois2,L{i},dataOrdered)
+       end
+        close(h)
+    end
+    disp('Summary...')
+    ReportGen2 (L,'Summary_report.xlsx') %Summary
+    disp('Extracting Correlation values for all subjects ...')
+    All_Corr = ReportGen3 (D.Z,I,Rois2,'CorrMat_allsubjects.xlsx');%All correlation matrices in one excel file
+    save('BW_results.mat','L','All_Corr')
+    disp('B/W Analysis is done successfully!')
+    
 else
 
     if parallel_flag == 1
@@ -95,7 +147,7 @@ else
             ReportGen1(FileName,Rois2,L{i},dataOrdered)
             end
             matlabpool close
-        end
+        end     
     else
         h = waitbar(0,'Calculating Metrics & Generating Reports ...');
         for i = 1 : nofSubj
